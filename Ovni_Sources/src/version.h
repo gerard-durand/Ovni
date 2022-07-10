1119aac4b6c384a7694ea6959a4b550af920cdb3
Gérard DURAND <gerard-florence.durand@orange.fr>
2022-07-10 18:56:13 +0200

    Utilisation de wxWidgets 3.2.0

--- Ovni_Sources/src/version.h
+++ Ovni_Sources/src/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "11";
-	static const char MONTH[] = "04";
+	static const char DATE[] = "07";
+	static const char MONTH[] = "06";
 	static const char YEAR[] = "2022";
-	static const char UBUNTU_VERSION_STYLE[] =  "22.04";
+	static const char UBUNTU_VERSION_STYLE[] =  "22.06";
 
 	//Software Status
 	static const char STATUS[] =  "Beta";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 5;
 	static const long MINOR  = 4;
-	static const long BUILD  = 13;
-	static const long REVISION  = 9968;
+	static const long BUILD  = 14;
+	static const long REVISION  = 10120;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1592;
-	#define RC_FILEVERSION 5,4,13,9968
-	#define RC_FILEVERSION_STRING "5, 4, 13, 9968\0"
-	static const char FULLVERSION_STRING [] = "5.4.13.9968";
+	#define RC_FILEVERSION 5,4,14,10120
+	#define RC_FILEVERSION_STRING "5, 4, 14, 10120\0"
+	static const char FULLVERSION_STRING [] = "5.4.14.10120";
 
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 32;
+	static const long BUILD_HISTORY  = 33;
 
 }
 #endif //VERSION_H
---
+++ wxWidgets-3.2.0/build/msw/00_GDs_64_maj.bat
@@ -0,0 +1,25 @@
+time /T > 00_test.txt
+REM setup.h dans lib\gcc_dll\mswu\wx modifié pour activer Direct2D
+REM Mettre MinGW64\bin avant MinGW32\bin
+set PATH=C:\MinGW64\bin;%PATH%
+ren C:\Utilitaires_Msys-Unix\bin\sh.exe shx.exe
+ren C:\msys64\usr\bin\sh.exe shx.exe
+REM <=> VENDOR=cb_64
+
+REM *** Version dll monolitique (-fno-keep-inline-dllexport indispensable pour gcc >= 4.6, mais uniquement sur version dll monolitique apparemment)
+REM echo _ >> log.txt
+REM echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* >> log.txt
+REM echo _ >> log.txt
+set CB_ID=cb_64
+
+echo %time% >> 00_test.txt
+mingw32-make -f makefile.gcc -j 4 SHARED=1 MONOLITHIC=1 BUILD=release VENDOR=%CB_ID% CXXFLAGS="-std=gnu++20"
+rem >log.txt 2>&1
+echo %time% >> 00_test.txt
+strip ..\..\lib\gcc_dll\wxmsw32u_gcc_%CB_ID%.dll
+strip ..\..\lib\gcc_dll\wxmsw32u_gl_gcc_%CB_ID%.dll
+echo %time% >> 00_test.txt
+
+ren C:\msys64\usr\bin\shx.exe sh.exe
+ren C:\Utilitaires_Msys-Unix\bin\shx.exe sh.exe
+time /T >> 00_test.txt
